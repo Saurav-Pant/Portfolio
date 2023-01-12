048a189799abe1f04f4cb55dd003ca6dde444f02
Saurav Pant <pantsaurav005@gmail.com>
2023-01-12 20:48:00 +0530

    Frustate lol

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,66 +8,62 @@ import { HiOutlineComputerDesktop } from "react-icons/hi2";
 import { TiContacts } from "react-icons/ti";
 import { GiHamburgerMenu, GiHiveMind } from "react-icons/gi";
 import { useState } from 'react';
-// import Toggle from '../Toggle/Toggle';
 
 
 const Navbar = () => {
-    const[toggle,setToggle]=useState('false');
+    const [showNav, setShowNav] = useState(false)
 
-    const forChange = ()=> {
-        console.log("ok");
-        toggle === 'false'?setToggle('true'):setToggle('false')
-    }
-  return (
+  
+    return (
         <>
-        <div className="Whole_Nav">
+            <div className="Whole_Nav">
+                <motion.div className="logo_pic"
+                    initial={{ x: 0, opacity: 0 }}
+                    animate={{ x: 100, opacity: 1 }}
+                    transition={{ ease: "easeInOut", duration: 1, delay: .6 }}
+                >
 
-            <motion.div className="logo_pic"
-              initial={{ x:0,opacity:0}}
-              animate={{ x:100,opacity: 1 }}
-              transition={{ ease: "easeInOut", duration: 1, delay:.6 }}
-            >
+                    <h1><Link to="/" className='same1'>SauraV</Link> </h1>
 
-             <h1><Link to="/" className='same1'>SauraV</Link> </h1>
+                </motion.div>
 
-            </motion.div>
+                {/* NavSection */}
+                {/* <Toggle/> */}
+                <div className="main_nav" id={showNav ? 'main_nav' : ""}>
+                    <ul style={{ listStyleType: "none" }} id="show" >
+                        <li>
+                            <Link to="/" className='same'>
+                                <FaHome className='icon' /> HOME</Link>
+                        </li>
 
-            {/* NavSection */}
-            {/* <Toggle/> */}
+                        <li>
+                            <Link to="/About" className='same' id="about"> <BsFileEarmarkPersonFill className='icon' />ABOUT</Link>
+                        </li>
 
-            <div className="main_nav">
-                <ul style={{ listStyleType: "none" }}>
-                    <li>
-                        <Link to="/" className='same'>                        
-                            <FaHome className='icon' /> HOME</Link>  
-                   </li>
+                        <li>
+                            <Link to="/Project" className='same' id='project'><HiOutlineComputerDesktop className='icon' />PROJECTS</Link>
+                        </li>
 
-                    <li>
-                        <Link to="/About" className='same' id="about"> <BsFileEarmarkPersonFill className='icon'/>ABOUT</Link>
-                    </li>
+                        <li>
+                            <Link to="/Skills" className='same' id='skill'> <GiHiveMind className='icon' />SKILLS</Link>
+                        </li>
 
-                    <li>
-                        <Link to="/Project" className='same' id='project'><HiOutlineComputerDesktop className='icon'/>PROJECTS</Link>
-                    </li>
+                        <li>
+                            <Link to="/Contact" className='same' id='contact'><TiContacts className='icon' />CONTACT</Link>
+                        </li>
 
-                    <li>
-                        <Link to="/Skills" className='same' id='skill'> <GiHiveMind className='icon'/>SKILLS</Link>
-                    </li>
+                    </ul>             
+                </div>
 
-                    <li>
-                        <Link to="/Contact" className='same' id='contact'><TiContacts className='icon'/>CONTACT</Link>
-                        
-                    </li>
 
-                </ul>
+
+                <div className='active'>
+                    <GiHamburgerMenu className='ham' onClick={()=>{setShowNav(!showNav)}} />
+                </div>
             </div>
-            <div className="active">
-            <GiHamburgerMenu className='ham' onClick={forChange}/>
-            </div>
-        </div>
 
         </>
-  )
+    )
 }
 
 export default Navbar
